f854d9df06824dd527a5a06457fbb0166bd8e866
nicolaiharbo <nicolai@famharbo.dk>
2016-05-05 17:52:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/blog/semesterproject/LoadBlogPostsAsyncTask.java
@@ -0,0 +1,19 @@
+package com.example.blog.semesterproject;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by nicolaiharbo on 05/05/2016.
+ */
+public class LoadBlogPostsAsyncTask extends AsyncTask<Void, Void, Void> {
+    @Override
+    protected Void doInBackground(Void... params) {
+        //REST KALD TIL DB, SKAL FYLDE ET ARRAY OP MED BLOGPOSTS
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+}
